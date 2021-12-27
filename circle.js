9931e8eccf47aa6390ecfa3ea2a7406b864e35eb
pradeep <dashwar.pradeep@gmail.com>
2021-12-28 04:01:19 +0530

    first commit

---
+++ circle.js
@@ -0,0 +1,18 @@
+import { Shape } from './shape.js';
+// const shape=ner Shape();
+export class ClassName { Circle };
+export class Circle extends Shape {
+    constructor() {
+        super();
+        // console.log("there is nothing in constructor");
+    }
+    calculateArea() {
+        // console.log("area of circle is pi*r*r");
+        // super.calculateArea();
+    }
+
+}
+// let circle = new Circle();
+// console.log(circle.calculateArea());
+// console.log(circle.drawShape());
+// console.log(circle.color);
---
+++ shape.js
@@ -0,0 +1,12 @@
+export class ClassName { Shape };
+export class Shape {
+    constructor() {
+        this.color = "red";
+    }
+    drawShape() {
+        // console.log("I am in drawShape");
+    }
+    calculateArea() {
+        // console.log("Area of a shape is decide by its dimensions");
+    }
+}
